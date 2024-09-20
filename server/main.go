7cdaef4e1eccb322e4aacb8c6a924bcd619a15fd
Cheemx <chinmaymahajan999@gmail.com>
2024-09-20 07:10:30 +0530

    port updated

--- server/main.go
+++ server/main.go
@@ -26,7 +26,7 @@ func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = ":8080"
+		port = "8080"
 		log.Fatal("Error in parsing the env")
 	}
 	fmt.Println("Server is getting started ...")
