923a0f49e0c25465575455eba901946c2e8144cd
Cheemx <chinmaymahajan999@gmail.com>
2024-09-20 17:48:43 +0530

    cors origins updated!

--- server/main.go
+++ server/main.go
@@ -25,7 +25,7 @@ func main() {
 	fmt.Println("Server is getting started ...")
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://chit-chat-green.vercel.app/"},
+		AllowedOrigins:   []string{"https://chit-chat-green.vercel.app/", "https://chit-chat-gl6z7gk86-cheemxs-projects.vercel.app", "https://chit-chat-git-main-cheemxs-projects.vercel.app"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"X-Requested-With", "Content-Type", "Authorization"},
