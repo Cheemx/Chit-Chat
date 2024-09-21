a702cfc684c4240eeb1e2956e809aafe3225a146
Cheemx <chinmaymahajan999@gmail.com>
2024-09-21 12:39:50 +0530

    loading class added!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,11 @@ function App() {
             <Outlet />
             <Footer />  
         </div>
-    ) : null
+    ) : (
+        <div className='flex items-center justify-center min-h-screen bg-black'>
+            <div className='w-16 h-16 border-8 border-t-cyan-500 border-gray-300 rounded-full animate-spin'></div>
+        </div>
+    )
 }
 
 export default App
