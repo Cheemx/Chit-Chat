5e9419dc4505854e9afcbc972757454781a9bd84
Cheemx <chinmaymahajan999@gmail.com>
2024-10-27 09:45:41 +0530

    updated msg Schema for file sharing!

--- server/model/message.go
+++ server/model/message.go
@@ -10,6 +10,9 @@ type Message struct {
 	SenderID    primitive.ObjectID  `bson:"senderId" json:"senderId" validate:"required"`
 	ReceiverID  primitive.ObjectID  `bson:"receiverId" json:"receiverId" validate:"required"`
 	MessageBody string              `bson:"message" json:"message" validate:"required"`
+	Type        string              `bson:"type" json:"type"`
+	FileURL     string              `bson:"fileUrl,omitempty" json:"fileUrl,omitempty"`
+	FileType    string              `bson:"fileType,omitempty" json:"fileType,omitempty"`
 	CreatedAt   time.Time           `bson:"createdAt" json:"createdAt" validate:"required"`
 	UpdatedAt   primitive.Timestamp `bson:"updatedAt" json:"updatedAt" validate:"required"`
 }
