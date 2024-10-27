35b9916faa28d04ab5254d56803c63ead76d87fa
Cheemx <chinmaymahajan999@gmail.com>
2024-10-27 09:44:48 +0530

    added cors allowance for dev server!

--- server/main.go
+++ server/main.go
@@ -8,10 +8,12 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 func init() {
+	godotenv.Load()
 	db.ConnectDB()
 }
 
@@ -25,7 +27,7 @@ func main() {
 	fmt.Println("Server is getting started ...")
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://chit-chat-green.vercel.app", "https://chit-chat-gl6z7gk86-cheemxs-projects.vercel.app", "https://chit-chat-git-main-cheemxs-projects.vercel.app"},
+		AllowedOrigins:   []string{"https://chit-chat-green.vercel.app", "http://localhost:5173"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"X-Requested-With", "Content-Type", "Authorization"},
